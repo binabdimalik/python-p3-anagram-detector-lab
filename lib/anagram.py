e4c43cedace4d464012dfd5e3aeb9ef0a2238499
Abdimalik <binabdimalikranger@gmail.com>
2025-11-30 15:14:01 +0300

    resolved

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        return [
+            candidate 
+            for candidate in possible_anagrams 
+            if sorted(candidate) == sorted(self.word)
+        ]
